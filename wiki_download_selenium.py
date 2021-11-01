52a39330c10604260559c4295e6b260f06660a93
Vicente <93003245+concursotp2021@users.noreply.github.com>
2021-11-01 23:53:03 +0100

    Update wiki_download_selenium.py

--- wiki_download_selenium.py
+++ wiki_download_selenium.py
@@ -5,13 +5,15 @@ import os
 import requests
 
 # En este fichero ponemos las urls de donde queremos descargar las imagenes
+#ejemplo: https://es.wikipedia.org/wiki/Melgar de Abajo
 f = open("lista_enlaces.txt","r")
 for line in f:
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(line)
     v_url = driver.find_element_by_xpath('//img[1]').get_attribute('src')
-    # En este fichero guardamos las urls de las imagenes que queremos
+    # En este fichero guardamos las urls que vamos encontrando
+    # Ejemplo: https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Escudo_de_Melgar_de_Abajo_%28Valladolid%29.svg/87px-Escudo_de_Melgar_de_Abajo_%28Valladolid%29.svg.png
     f2 = open ("lista_enlaces_salida.txt","a")
     f2.write(v_url+"\n")
     f2.close
