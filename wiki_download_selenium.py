7bb7cb5d2017de34084b77b39cbb1121bba18c12
Vicente <93003245+concursotp2021@users.noreply.github.com>
2021-11-01 23:50:35 +0100

    Update wiki_download_selenium.py

--- wiki_download_selenium.py
+++ wiki_download_selenium.py
@@ -4,18 +4,19 @@ from fp.fp import FreeProxy
 import os
 import requests
 
+# En este fichero ponemos las urls de donde queremos descargar las imagenes
 f = open("lista_enlaces.txt","r")
 for line in f:
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(line)
     v_url = driver.find_element_by_xpath('//img[1]').get_attribute('src')
+    # En este fichero guardamos las urls de las imagenes que queremos
     f2 = open ("lista_enlaces_salida.txt","a")
-    #print(driver.find_element_by_xpath('//img[1]').get_attribute('src'))
     f2.write(v_url+"\n")
     f2.close
     driver.close()
 
 f.close()
-print("finish")
+print("se acabo lo vailao")
 
