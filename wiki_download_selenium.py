c778af5eccd80d1ce6467cafce62b6aa15795cc8
Vicente <93003245+concursotp2021@users.noreply.github.com>
2021-11-01 23:41:13 +0100

    Update wiki_download_selenium.py

--- wiki_download_selenium.py
+++ wiki_download_selenium.py
@@ -5,13 +5,17 @@ import os
 import requests
 
 f = open("lista_enlaces.txt","r")
-for x in f:
+for line in f:
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(x)
-    print(driver.find_element_by_xpath('//img[1]').get_attribute('src'))
+    driver.get(line)
     v_url = driver.find_element_by_xpath('//img[1]').get_attribute('src')
-
+    f2 = open ("lista_enlaces_salida.txt","a")
+    #print(driver.find_element_by_xpath('//img[1]').get_attribute('src'))
+    f2.write(v_url+"\n")
+    f2.close
     driver.close()
 
-f.close()    
+f.close()
+print("finish")
+
